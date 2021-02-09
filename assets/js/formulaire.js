4bb74aad89a0cd8551f0813627de5a8d08c72a38
Mes Morts <titouanescorneboueu@gmail.com>
2021-02-09 18:48:10 +0100

    bordel

---
+++ assets/js/formulaire.js
@@ -0,0 +1,56 @@
+// ! Initialisation des const
+const todoListe = document.querySelector('.liste');
+const todoTache = document.querySelector('#tache');
+const todoButton = document.querySelector("#bouton");
+const msg = document.querySelector('.msg');
+
+// ? Au clique du bouton!
+todoButton.addEventListener('click', (e) =>{
+    e.preventDefault()
+    
+    const l = document.createElement('li');
+    l.className = 'li';
+    l.appendChild(document.createTextNode(`${todoTache.value}`));
+
+    if(todoTache.value === "") {
+        msg.innerHTML = 'Ho tu écrit quelque chose!!!';
+        msg.style.background = 'red';
+        setTimeout(() =>{
+            msg.innerText = "";
+            msg.style.background = 'transparent'
+        }, 2500);
+    }else {
+        const todoCheck = document.createElement('button');
+        const todoRemove = document.createElement('button');
+        todoCheck.innerHTML = '<p> OK </p>';
+        todoRemove.innerHTML = '<p> NO </p>';
+        todoCheck.classList.add('check');
+        todoRemove.classList.add('remove');
+        l.appendChild(todoCheck);
+        l.appendChild(todoRemove);
+        todoTache.value = "";
+
+    }
+    console.log(l);
+});
+
+// ?Bouton check et remove
+todoListe.addEventListener('click', (e) =>{
+    const button = e.target.parentNode;
+
+    if(button.classList.contains('remove')) {
+        msg.innerText = 'Baltringue pourquoi tu as supprimé ça!'
+        msg.style.background = 'yellow';
+        setTimeout(() =>{
+            msg.innerText = "";
+            msg.style.background = 'transparent'
+        }, 2500);
+        button.parentNode.remove();
+    }else if(button.classList.contains('check')) {
+        if(button.parentNode.style.textDecoration === 'line-through') {
+            button.parentNode.style.textDecoration = 'none';
+        } else {
+            button.parentNode.style.textDecoration = 'line-through';
+        }
+    };
+});
